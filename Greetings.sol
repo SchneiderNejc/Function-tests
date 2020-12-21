6d10413f7f481c7e21f90c8b2f5d5095b1cd784b
Nejc Schneider <nejc.sch@gmail.com>
2020-12-21 17:04:26 +0100

    Add smart contract

---
+++ Greetings.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.18;
+contract Greetings{
+  string message;
+
+  function Greetings() public{
+    message = "I'm ready!";
+  }
+  function setMessage(string _message) public{
+    message = _message;
+  }
+  function getMessage() public view returns(string){
+    return message;
+  }
+}
