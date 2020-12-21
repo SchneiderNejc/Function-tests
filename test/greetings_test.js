08c85f12a86421fbc6d035dcba74c94887ac94e3
SchneiderNejc <nejc.sch@gmail.com>
2020-12-21 20:36:11 +0100

    fixed test

--- test/greetings_test.js
+++ test/greetings_test.js
@@ -5,11 +5,12 @@ contract('Greetings', function(accounts){
 
 
   before('set up instance for each test', async() =>{
-    greetingsInstance = await Greeting.deployed();
+    greetingsInstance = await Greetings.deployed();
   });
-  it('should be able to change message', async() =>{
-    greetingsInstance.setMessage('Hello dear');
-    const message = greetingsInstance.getMessage();
-    assert.equal(message, 'Hello dear');
+
+  it('should be able to change message', async () =>{
+    await greetingsInstance.setMessage('Hello dear');
+    const message = await greetingsInstance.getMessage();
+    assert.equal(message, 'Hello dear', 'Message must be: ', message);
   });
 });
